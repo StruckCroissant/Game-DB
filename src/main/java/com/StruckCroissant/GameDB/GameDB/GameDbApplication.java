dedf14e69bfa0d9f173fed4c94ff7d8287b855b0
StruckCroissant <dakotaarduino@gmail.com>
2022-04-24 20:54:44 -0500

    Init Spring file-structure. Some simple controller testing included

---
+++ src/main/java/com/StruckCroissant/GameDB/GameDB/GameDbApplication.java
@@ -0,0 +1,13 @@
+package com.StruckCroissant.GameDB.GameDB;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GameDbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GameDbApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/StruckCroissant/GameDB/GameDB/GameDbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.StruckCroissant.GameDB.GameDB;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GameDbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
