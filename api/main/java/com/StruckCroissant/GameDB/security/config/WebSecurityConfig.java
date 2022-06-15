a5ebe5321f65a18c4ccc1cffeb1a226c805d3fcc
StruckCroissant <dakotaarduino@gmail.com>
2022-06-14 22:22:05 -0500

    Added login request endpoint to security config

--- api/main/java/com/StruckCroissant/GameDB/security/config/WebSecurityConfig.java
+++ api/main/java/com/StruckCroissant/GameDB/security/config/WebSecurityConfig.java
@@ -32,8 +32,8 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter  {
         http
                 .csrf().disable()
                 .authorizeRequests()
-                    .antMatchers("/api/v*/register/**")
-                    .permitAll()
+                    .antMatchers("/api/v*/register/**").anonymous() // Difference between anonymous & permit all?
+                    .antMatchers("/api/v*/login/**").permitAll()
                 .anyRequest()
                 .authenticated();
 
