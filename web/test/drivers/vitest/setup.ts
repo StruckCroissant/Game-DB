e867170ecf8bea3d685084f8868412bea548ab17
StruckCroissant <dakotaarduino@gmail.com>
2023-12-17 22:43:16 -0600

    WIP creating example tests

---
+++ web/test/drivers/vitest/setup.ts
@@ -0,0 +1,13 @@
+import matchers from "@testing-library/jest-dom/matchers";
+import { afterAll, afterEach, beforeAll, expect } from "vitest";
+import { mockServer } from "../../mock-server";
+
+expect.extend(matchers);
+
+beforeAll(() => mockServer.listen({ onUnhandledRequest: "error" }));
+afterAll(() => mockServer.close());
+afterEach(() => {
+  mockServer.resetHandlers();
+  localStorage.clear();
+  sessionStorage.clear();
+});
