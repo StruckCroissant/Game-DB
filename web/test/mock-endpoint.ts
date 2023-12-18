060c16565a3fca8c8f70f4b247b8961c0eb5adaa
StruckCroissant <dakotaarduino@gmail.com>
2023-12-17 21:21:12 -0600

    Fixed typescript build errors

---
+++ web/test/mock-endpoint.ts
@@ -0,0 +1,28 @@
+import { SetupWorker } from "msw/browser";
+import { SetupServer } from "msw/node";
+import { HttpResponse, http } from "msw";
+
+type MockServer = { mockServer: SetupServer | SetupWorker };
+
+export const makeMockEndpoint =
+  ({ mockServer }: MockServer) =>
+  (
+    path: string,
+    {
+      body,
+      method = "get",
+      status = 200,
+    }: {
+      body: string | unknown[] | Record<string | number, unknown>;
+      method?: "get" | "post" | "put" | "patch" | "delete";
+      status?: number;
+    }
+  ) => {
+    mockServer.use(
+      http[method](path, () =>
+        HttpResponse.json(body, {
+          status: status,
+        })
+      )
+    );
+  };
