41f9379567973c614965591ed129f6125c6d6c2d
StruckCroissant <32440863+StruckCroissant@users.noreply.github.com>
2024-02-10 22:11:05 -0600

    Removed data-testid attributes from the webpage

--- web/vite.config.ts
+++ web/vite.config.ts
@@ -4,16 +4,43 @@ import vue from "@vitejs/plugin-vue";
 import istanbul from "vite-plugin-istanbul";
 import type { UserConfig } from "vite";
 
-// https://vitejs.dev/config/
-export default function config() {
-  const config: UserConfig = {
-    plugins: [vue()],
-    resolve: {
-      alias: {
-        "@": fileURLToPath(new URL("./src", import.meta.url)),
+const vuePlugin = () =>
+  vue({
+    template: {
+      compilerOptions: {
+        nodeTransforms: [
+          (node) => {
+            if (process.env.NODE_ENV !== "production") return;
+            if (node.type !== 1 /*NodeTypes.ELEMENT*/) return;
+            for (let i = 0; i < node.props.length; i++) {
+              const prop = node.props[i];
+              if (
+                !prop ||
+                prop.type !== 6 /*NodeTypes.ELEMENT*/ ||
+                prop.name !== "data-testid"
+              )
+                continue;
+              node.props.splice(i, 1);
+              i--;
+            }
+          },
+        ],
       },
     },
-  };
+  });
+
+const defaultConfig: () => UserConfig = () => ({
+  plugins: [vuePlugin()],
+  resolve: {
+    alias: {
+      "@": fileURLToPath(new URL("./src", import.meta.url)),
+    },
+  },
+});
+
+// https://vitejs.dev/config/
+export default function config(): UserConfig {
+  const config = { ...defaultConfig() };
 
   if (process.env.INSTRUMENT_BUILD) {
     config["build"] = {
