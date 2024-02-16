37c3bc23475b74e941cc934716f64bd812915303
StruckCroissant <32440863+StruckCroissant@users.noreply.github.com>
2024-02-15 20:30:49 -0600

    Added register component tests

--- web/src/services/network/__mocks__/authenticationHttp.ts
+++ web/src/services/network/__mocks__/authenticationHttp.ts
@@ -3,8 +3,14 @@ import { useFetch } from "@/composables/network/useFetch";
 vi.mock("@/composables/network/useFetch");
 
 const useLoginMocks = {
-  ...useFetch(),
+  ...useFetch("/login"),
   doLogin: vi.fn(),
 };
 
+const useRegisterMocks = {
+  ...useFetch("/register"),
+  doRegister: vi.fn(),
+};
+
 export const useLogin = () => useLoginMocks;
+export const useRegister = () => useRegisterMocks;
